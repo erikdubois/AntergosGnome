406eecc2b6d80e90b3faf25eabb10b3896a5da5a
Erik Dubois <erik.dubois@gmail.com>
2016-06-30 20:19:36 +0200

    Commit comment : update on Thu 30 Jun 20:19:36 CEST 2016

---
+++ hardcodefixer.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+#              `.-/::/-``
+#            .-/osssssssso/.               
+#           :osyysssssssyyys+-              
+#        `.+yyyysssssssssyyyyy+.           
+#       `/syyyyyssssssssssyyyyys-`         
+#      `/yhyyyyysss++ssosyyyyhhy/`         
+#     .ohhhyyyyso++/+oso+syy+shhhho.       
+#    .shhhhysoo++//+sss+++yyy+shhhhs.      
+#   -yhhhhs+++++++ossso+++yyys+ohhddy:     
+#  -yddhhyo+++++osyyss++++yyyyooyhdddy-    
+# .yddddhso++osyyyyys+++++yyhhsoshddddy`   
+#`odddddhyosyhyyyyyy++++++yhhhyosddddddo   
+#.dmdddddhhhhhhhyyyo+++++shhhhhohddddmmh.  
+#ddmmdddddhhhhhhhso++++++yhhhhhhdddddmmdy  
+#dmmmdddddddhhhyso++++++shhhhhddddddmmmmh  
+#-dmmmdddddddhhyso++++oshhhhdddddddmmmmd- 
+# .smmmmddddddddhhhhhhhhhdddddddddmmmms. 
+#   `+ydmmmdddddddddddddddddddmmmmdy/.     
+#      `.:+ooyyddddddddddddyyso+:.`
+#
+##################################################################################################################
+#
+# Current project : AntergosGnome
+#
+# Source 	: 	https://github.com/erikdubois/AntergosGnome
+#
+##################################################################################################################
+#
+# Written to be used on 64 bits computers
+# Author 	: 	Erik Dubois
+# Website 	: 	http://www.erikdubois.be
+#
+##################################################################################################################
+# 
+#
+# More from Erik Dubois
+#
+# Aurora Conky
+# at http://sourceforge.net/projects/auroraconkytheme/
+# Explanation on the use of this theme can be found at 
+# http://erikdubois.be/category/linux/aurora-conky/
+# 
+# Aureola Conky
+# Collections of nice conky's with lua syntax
+# https://github.com/erikdubois/Aureola
+#
+# Sardi icons
+# Many different styles of icons from colourfull, monochrome, white, circle
+# https://sourceforge.net/projects/sardi/
+#
+# Super Ultra Flat Numix Remix
+# Colourfull and playfull icons
+# https://github.com/erikdubois/Super-Ultra-Flat-Numix-Remix
+#
+# Check out the github - many more scripts for automatic installation of Linux Systems.
+#
+#
+#
+##################################################################################################################
+#
+#   DO NOT JUST RUN THIS. EXAMINE AND JUDGE. AT YOUR OWN RISK.
+#
+##################################################################################################################
+#
+#
+##################################################################################################################
+# If the option -S has been added. It will autoinstall all. Omit if you do not want that.
+##################################################################################################################
+#
+#
+git clone https://github.com/Foggalong/hardcode-fixer /tmp/hardcode-fixer
+sudo pacman -S curl
+sudo /tmp/hardcode-fixer/fix.sh
+
+
+
+echo "################################################################"
+echo "###################    T H E   E N D      ######################"
+echo "################################################################"
